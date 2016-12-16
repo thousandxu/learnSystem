a3c284f8274a5b22a4842ed2664a0de40c7f3e41
yiqianxu <thousandxu@163.com>
2016-12-16 16:38:13 +0800

    thousand

---
+++ app/scripts/app.js
@@ -0,0 +1,11 @@
+angular.module('BlankApp',[
+
+])
+.config(['$stateProvider','$urlRouterProvider','cfpLoadingBarProvider',function($stateProvider,$urlRouterProvider,cfpLoadingBarProvider){
+	// $stateProvider.state('eventlist',{
+  //   		url: '/eventlist',
+  //   		templateUrl: 'view/eventlist.html',
+  //       controller: 'EventListCtrl'
+  //   	})
+
+}]);
---
+++ app/scripts/controllers/TopMenuController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* menuController */
+
+var app = angular.module('BlankApp', []);
+// angular.module('BlankApp')
+app.controller('TopMenuCtrl', ['$rootScope', '$scope', '$http','$location',
+      function($rootScope, $scope, $http,$location){
+
+}])
