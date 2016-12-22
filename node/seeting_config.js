7e02b8f99db22de2966cfc4894c537e7746f3e2c
yiqianxu <thousandxu@163.com>
2016-12-22 20:07:28 +0800

    thousand

---
+++ node/seeting_config.js
@@ -0,0 +1,19 @@
+var setting={
+	mailconfig:{
+		 // sender_mail:'report@sailingcloud.cn',
+		 // sender_pass:'Abcd1234',
+		 // mailServer:'smtp.mxhichina.com',
+		 // receivers:'dreamlinkjiang@163.com,sohach@163.com',
+		 // cc:'changcheng316@zju.edu.cn'
+	},
+	mysqlconfig:{
+		 // acquireTimeout: 30000,
+		 // connectionLimit : 10,
+		 // host: '10.10.101.107',
+		 // user: 'buynow',
+		 // password: 'buynow',
+		 // database:'apm_dev',
+		 // port: 3306
+	}
+}
+module.exports=setting;
